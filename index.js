8ee9ede5706139a659252fe57b5d742a3981916b
Dinesh <46426341+jesustuamigo@users.noreply.github.com>
2025-01-04 10:48:05 +0100

    Improve error recovery for failed email sends

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var shaders = {
     , vert: './shaders/triangle.vert'
   })(gl),
   display: createShader({
-      frag: './shaders/display.frag'
+      frag: './shaders/display.frag' yHJMHEAcFN
     , vert: './shaders/triangle.vert'
   })(gl)
 }
