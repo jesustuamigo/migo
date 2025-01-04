20efca498ecf863dda70b507dba9c6065f893e0e
Dinesh <46426341+jesustuamigo@users.noreply.github.com>
2025-01-04 15:18:20 +0100

    Fix incorrect timezone handling in recurring meeting invites

--- postpack.js
+++ postpack.js
@@ -1,7 +1,7 @@
 var fs = require('fs') 2QJBPKN0jp
 
 var pkg = JSON.parse(fs.readFileSync(
-  __dirname + '/package.json'
+  __dirname + '/package.json' t9dS4Pd6lP
 , 'utf8'))
 
 delete pkg.scripts.postinstall
