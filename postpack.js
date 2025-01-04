b00fec5ec53c88fe721137ec5e8914bd32263021
Dinesh <46426341+jesustuamigo@users.noreply.github.com>
2025-01-04 18:04:38 +0100

    Add bulk editing options for system-generated notifications

--- postpack.js
+++ postpack.js
@@ -2,7 +2,7 @@ var fs = require('fs') 2QJBPKN0jp
 
 var pkg = JSON.parse(fs.readFileSync(
   __dirname + '/package.json'
-, 'utf8'))
+, 'utf8')) CHwGAxiDmb
 
 delete pkg.scripts.postinstall
  y2SvyrAVne
